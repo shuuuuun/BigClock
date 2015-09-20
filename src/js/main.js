8a551e8b0bbf7acc0fa5843b3685a7b23e2dacd1
motoki-shun <motoki-shun@kayac.com>
2015-09-20 17:14:45 +0900

    Implementation

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,25 @@
       $(".onlysp").remove();
     }
     
+    var $clock = $(".clock");
+    var $date = $(".date");
+    
+    setInterval(function(){
+      var now = new Date();
+      var hour = ("0" + now.getHours()).slice(-2);
+      var min = ("0" + now.getMinutes()).slice(-2);
+      var sec = ("0" + now.getSeconds()).slice(-2);
+      $clock.text(hour+":"+min+":"+sec);
+    }, 100);
+    
+    var dayChara = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = ("0" + (now.getMonth()+1)).slice(-2);
+    var date = ("0" + now.getDate()).slice(-2);
+    var day = now.getDay();
+    $date.text(year+"."+month+"."+date+" "+dayChara[day]);
+    
   });
   
   // for development

6a6977335c753b8e9e33d2c931988af82217eae0
motoki-shun <motoki-shun@kayac.com>
2016-07-18 08:56:36 +0900

    DOMでアナログ時計

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,21 @@
 (function(win, doc, ns){
   
   var INTERVAL = 500;
-  var $clock, $date;
+  var $clock, $analog, $date;
+  var $handHour, $handMin, $handSec;
   var dayChara = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
   
   window.onload = function(){
     $clock = document.querySelector(".clock");
+    $analog = document.querySelector(".analog");
     $date = document.querySelector(".date");
     
     setInterval(function(){
       printTime();
+      printAnalog();
     }, INTERVAL);
+    
+    setupAnalog();
     
     printTime();
     printDate();
@@ -34,6 +39,34 @@
     var date = ("0" + now.getDate()).slice(-2);
     var day = now.getDay();
     $date.innerText = year+"."+month+"."+date+" "+dayChara[day];
+  }
+  
+  function setupAnalog(){
+    $handHour = document.createElement('span');
+    $handMin = document.createElement('span');
+    $handSec = document.createElement('span');
+    $handHour.classList.add('hand-hour');
+    $handMin.classList.add('hand-min');
+    $handSec.classList.add('hand-sec');
+    for (var i = 0; i < 12; i++) {
+      var $num = document.createElement('span');
+      $num.classList.add('num');
+      $num.innerText = i + 1;
+      $analog.appendChild($num);
+    }
+    $analog.appendChild($handHour);
+    $analog.appendChild($handMin);
+    $analog.appendChild($handSec);
+  }
+  
+  function printAnalog(){
+    var now = new Date();
+    var hour = now.getHours();
+    var min = now.getMinutes();
+    var sec = now.getSeconds();
+    $handHour.style.transform = 'rotate(' + (hour / 12 * 360) + 'deg)';
+    $handMin.style.transform = 'rotate(' + (min / 60 * 360) + 'deg)';
+    $handSec.style.transform = 'rotate(' + (sec / 60 * 360) + 'deg)';
   }
   
 })(this, document, App);
